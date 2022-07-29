7d45ba9d60793b03d22ec3d88b56813d34c14966
Yrol <yrollgayanth@yahoo.com>
2022-07-29 21:50:09 +1000

    Adding the test case for clicking on a search result

--- cypress/e2e/3-custom/nab.cy.js
+++ cypress/e2e/3-custom/nab.cy.js
@@ -25,11 +25,9 @@ describe('Accessing NAB website', () => {
         // Getting the results count of the first tab and if > 10 do an item count
         cy.get('.tabs-navigation-container > ul > li').first().then(($listItem) => {
 
-            cy.wrap($listItem),should('have.class', 'item-count')
-
             cy.wrap($listItem).find('.item-count').then(($span) => {
                 const $resultCount = $span.text()
-                
+
                 // const $searchResults = cy.get('.search-results-container .search-result-container')
                 const $searchResults = cy.get('.search-results-container').find('.search-result-container');
                 
@@ -37,6 +35,22 @@ describe('Accessing NAB website', () => {
                     $searchResults.should('have.length', 10)
                 } else {
                     $searchResults.should('be.lt', 10)
+                }
+            })
+        })
+    })
+
+    it('will select the second tab and click on the first search result if results are available', () => {
+
+        const $secondTabSelector = cy.get('.tabs-navigation-container > ul > li').eq(1)
+
+        $secondTabSelector.then(($secondTab) => {
+            cy.wrap($secondTab).find('.item-count').then(($itemCountSpan) => {
+                const $resultCount = $itemCountSpan.text()
+
+                if(parseInt($resultCount) >= 1) {
+                    $secondTabSelector.click()
+                    cy.get('.search-results-container').find('.search-result-container .search-result-heading').eq(0).click()
                 }
             })
         })
